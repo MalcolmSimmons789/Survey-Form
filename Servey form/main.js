daeaed99d14ef4eafd724b594a68ab94ed6e3543
MalcolmSimmons789 <Malcolm.Simmons789@yahoo.com>
2022-06-05 01:28:23 -0400

    Finished

--- Servey form/main.js
+++
@@ -1,9 +0,0 @@
-let submitListener = document.getElementById('submitListener');
-
-submitListener.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  let firstName = document.getElementById('first-name').value;
-
-  console.log(firstName);
-});
